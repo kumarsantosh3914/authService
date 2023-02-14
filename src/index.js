9cde7890518460dbc8c658aff27adb6a00ce1003
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-02-15 01:20:23 +0530

    Added get user by id in reop

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const aipRoutes = require('./routes/index');
 
+// const UserRepository = require('./repository/user-repository');
+
 const app = express();
 
 const prepareAndStartServer = () => {
@@ -12,8 +14,11 @@ const prepareAndStartServer = () => {
     app.use(bodyParser.urlencoded({ extended: true }));
 
     app.use('/api', aipRoutes);
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server Started on Port: ${PORT}`);
+        // const repo = new UserRepository();
+        // const response = await repo.getById(1);
+        // console.log(response);
     });
 };
 
